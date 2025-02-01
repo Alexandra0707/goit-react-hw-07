3d8c80f0bfc7a5a0e5b32f2f4b4a2e6f59a0561b
Alexandra0707 <alex07marta@gmail.com>
2025-02-01 13:56:20 +0000

    fist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,23 @@
-import { useState, useEffect } from "react";
 import "./App.css";
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
-import initialContacts from "../../assets/contacts.json";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "../../redux/contactsOps";
 
 function App() {
-  const [contacts, setContacts] = useState(
-    () => JSON.parse(localStorage.getItem("saved-contact")) ?? initialContacts
-  );
-  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
 
-  useEffect(
-    () => localStorage.setItem("saved-contact", JSON.stringify(contacts)),
-    [contacts]
-  );
-
-  const addContact = (newContact) => {
-    setContacts((prev) => [...prev, newContact]);
-  };
-
-  const deleteContact = (contactsId) => {
-    setContacts((prev) => prev.filter(({ id }) => id !== contactsId));
-  };
-  const handleSearchChange = (value) => {
-    setSearch(value.trim());
-  };
-
-  const findContact = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(search.toLowerCase())
-  );
-
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm addContacts={addContact} />
-      <SearchBox onSearch={search} setSearch={handleSearchChange} />
-      <ContactList contacts={findContact} deleteContact={deleteContact} />
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
     </div>
   );
 }
